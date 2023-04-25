582ef78161f3c5cb5ecf4db89b43a9d4485e7f9a
Andreja <andreja992@proton.me>
2023-04-25 22:19:57 +0200

    Implement page reset logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 // generate random number between 1 and 20
- const randao = Math.trunc(Math.random() * 20) + 1;
+ let randao = Math.trunc(Math.random() * 20) + 1;
 
  // create variable that will store remaining attempts
 let attemptRemaining = 20;
@@ -13,6 +13,10 @@ document.querySelector('.check').addEventListener('click', function(){
     if (guess === randao) {
         msg = 'Correct Number!';
         document.querySelector('.number').textContent = randao;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    } else if (!guess) {
+        msg = 'No Number!';
     } else if (attemptRemaining > 1) {
         if (guess < randao) {
             msg = 'Number too low!';
@@ -28,3 +32,15 @@ document.querySelector('.check').addEventListener('click', function(){
     document.querySelector('.score').textContent = attemptRemaining;
 });
 
+// restore page to initial state (restart game)
+document.querySelector('.again').addEventListener('click', function(){
+    randao = Math.trunc(Math.random() * 20) + 1;
+    attemptRemaining = 20;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = attemptRemaining;
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+});
