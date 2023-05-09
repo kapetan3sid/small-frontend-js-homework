59f454d720c392b5c2b2f1d3eb59de23c625e505
kapetan3sid <andreja992@proton.me>
2023-05-09 19:08:04 +0200

    modal

--- modal/modal.js
+++ modal/modal.js
@@ -1,1 +1,27 @@
+'use strict';
+// new project
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnOpen = document.querySelectorAll('.show-modal');
 
+// define variable that holds function that allows user to open modal
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+// allow user to open modals by removing "hidden" in HTML class name
+for (let i = 0; i < btnOpen.length; i++) {
+    btnOpen[i].addEventListener('click', openModal);
+};
+
+// define variable that holds function that allows user to close modal
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+// X button click close modal
+btnClose.addEventListener('click', closeModal);
+// overlay click close modal
+overlay.addEventListener('click', closeModal)
