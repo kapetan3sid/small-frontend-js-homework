d09b40b33b729267fb1f03f630530cfdaa68e46d
kapetan3sid <andreja992@proton.me>
2023-05-12 21:10:37 +0200

    reseting all the values and hiding all the modals

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,1 +1,10 @@
 'use strict';
+// select elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+// set scores to 0
+score0.textContent = 0;
+score1.textContent = 0;
+// hide dice
+let dice = document.querySelector('.dice')
+dice.classList.add('hidden');
