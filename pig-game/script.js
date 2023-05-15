ea9859f73ce9b2f137ea9713333da26ce0177e0a
kapetan3sid <andreja992@proton.me>
2023-05-15 23:00:02 +0200

    add winner criteria and DOM manipulation

--- pig-game/script.js
+++ pig-game/script.js
@@ -22,17 +22,19 @@ let currentScore = 0;
 let playerNum = 0;
 let hold0 = 0;
 let hold1 = 0;
+let gameRunning = true;
 
 //rolling dice function
 bntRoll.addEventListener('click', function() {
-    const diceNum = Math.trunc(Math.random() * 6) + 1;
-    dice.src = `dice-${diceNum}.png`;
-    dice.classList.remove('hidden');
-    // check if dice is 1
-    if (diceNum != 1) {
+    if (gameRunning) {
+        const diceNum = Math.trunc(Math.random() * 6) + 1;
+        dice.src = `dice-${diceNum}.png`;
+        dice.classList.remove('hidden');
+        // check if dice is 1
+        if (diceNum != 1) {
         currentScore = currentScore + diceNum;
         document.getElementById(`current--${playerNum}`).textContent = currentScore;
-    } else {
+        } else {
         document.getElementById(`current--${playerNum}`).textContent = 0;
         if (playerNum === 0) {
             playerNum = 1;
@@ -42,23 +44,40 @@ bntRoll.addEventListener('click', function() {
         currentScore = 0;
         player0.classList.toggle('player--active');
         player1.classList.toggle('player--active');
+    }} else {
+
     }
 });
 // holding the score and switching to other player
 bntHold.addEventListener('click', function() {
-    document.getElementById(`current--${playerNum}`).textContent = 0;
-    if (playerNum === 0) {
+    // check if game is not finished
+    if (gameRunning) {
+        document.getElementById(`current--${playerNum}`).textContent = 0;
+        if (playerNum === 0 ) {
         hold0 = hold0 + currentScore;
         score0.textContent = hold0;
         playerNum = 1;
+        if (hold0 >= 20) {
+            playing = false;
+            document.querySelector('.player--0').classList.add('player--winner');
+            document.querySelector('.player--0').classList.remove('player--active');
+            dice.classList.add('hidden');
+        };
     } else {
         hold1 = hold1 + currentScore;
         score1.textContent = hold1;
-        playerNum = 0; 
-    }
+        playerNum = 0;
+        if (hold1 >= 20) {
+            playing = false;
+            document.querySelector('.player--1').classList.add('player--winner');
+            document.querySelector('.player--1').classList.remove('player--active');
+            dice.classList.add('hidden');
+        };
+    };
     currentScore = 0;
     player0.classList.toggle('player--active');
     player1.classList.toggle('player--active');
+}
 });
 // reset all values to starting values
 btnNew.addEventListener('click', function() {
@@ -69,6 +88,11 @@ btnNew.addEventListener('click', function() {
     playerNum = 0;
     hold0 = 0;
     hold1 = 0;
+    playing = true;
     document.getElementById(`current--0`).textContent = 0;
     document.getElementById(`current--1`).textContent = 0;
+    document.querySelector('.player--0').classList.remove('player--winner');
+    document.querySelector('.player--0').classList.add('player--active');
+    document.querySelector('.player--1').classList.remove('player--winner');
+    document.querySelector('.player--1').classList.remove('player--active');
 })
